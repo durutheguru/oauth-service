02455b09f868c0e8bb9cc26bf53f9307bc2a5633
Julian Duru <durutheguru@gmail.com>
2022-04-24 21:54:15 +0100

    - New Endpoint for Fetching Registered clients

--- src/main/java/com/julianduru/oauthservice/graphql/client/ClientQueryResolver.java
+++ src/main/java/com/julianduru/oauthservice/graphql/client/ClientQueryResolver.java
@@ -1,19 +1,25 @@
 package com.julianduru.oauthservice.graphql.client;
 
+import com.julianduru.oauthservice.graphql.model.PagedClientResponse;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * created by julian on 22/04/2022
  */
 @Component
+@RequiredArgsConstructor
 public class ClientQueryResolver implements GraphQLQueryResolver {
 
 
-    public List<String> fetchClients() {
-        return List.of("client-one");
+    private final RegisteredClientRepository clientRepository;
+
+
+    public PagedClientResponse fetchClients(int page, int size) {
+        //TODO: possible implementation for fetching clients...
+        return PagedClientResponse.empty();
     }
 
 
