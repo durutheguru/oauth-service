c3e03212cc8dd35fff92d39cd353bf1510e717d5
Julian Duru <durutheguru@gmail.com>
2022-04-25 18:08:34 +0100

    - Added GQL Endpoint for fetching resource servers

--- src/main/java/com/julianduru/oauthservice/module/resourceserver/ResourceServerServiceImpl.java
+++ src/main/java/com/julianduru/oauthservice/module/resourceserver/ResourceServerServiceImpl.java
@@ -3,23 +3,34 @@ package com.julianduru.oauthservice.module.resourceserver;
 import com.julianduru.oauthservice.dto.CreateResourceServerRequest;
 import com.julianduru.oauthservice.entity.ResourceServer;
 import com.julianduru.oauthservice.module.resourceserver.component.CreateResourceServerComponent;
+import com.julianduru.oauthservice.repository.ResourceServerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 /**
  * created by julian on 24/04/2022
  */
 @Service
-@RequiredArgsConstructor
-public class ResourceServerServiceImpl implements ResourceServerService {
-
-
-    private final CreateResourceServerComponent createResourceServerComponent;
+public record ResourceServerServiceImpl(
+    ResourceServerRepository resourceServerRepository,
+    CreateResourceServerComponent createResourceServerComponent
+) implements ResourceServerService {
 
 
     @Override
     public ResourceServer createResourceServer(CreateResourceServerRequest request) {
         return createResourceServerComponent.newResourceServer(request);
+    }
+
+
+    @Override
+    public Page<ResourceServer> fetchResourceServers(int page, int size) {
+        return resourceServerRepository.findAll(
+            PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "resourceServerId"))
+        );
     }
 
 
