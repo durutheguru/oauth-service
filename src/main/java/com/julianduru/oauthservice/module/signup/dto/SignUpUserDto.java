45f376b25911fb7beeb6639e751504e5275dcbbc
Julian Duru <durutheguru@gmail.com>
2022-09-26 15:46:47 +0100

    - Updates to signup Controller endpoint

---
+++ src/main/java/com/julianduru/oauthservice/module/signup/dto/SignUpUserDto.java
@@ -0,0 +1,53 @@
+package com.julianduru.oauthservice.module.signup.dto;
+
+import com.julianduru.oauthservice.dto.UserDataDto;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * created by julian on 24/09/2022
+ */
+@Data
+public class SignUpUserDto {
+
+
+    private String username;
+
+
+    private String password;
+
+
+    private String confirm;
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+    private String email;
+
+
+    private List<String> authorities;
+
+
+    public UserDataDto toUserDataDto() {
+        var dto = new UserDataDto();
+
+        dto.setUsername(getUsername());
+        dto.setPassword(getPassword());
+        dto.setFirstName(getFirstName());
+        dto.setLastName(getLastName());
+        dto.setEmail(getEmail());
+        dto.setAuthorities(getAuthorities());
+
+        return dto;
+    }
+
+
+
+}
+
+
